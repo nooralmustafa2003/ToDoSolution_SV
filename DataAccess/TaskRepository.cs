e5d640620e006ba82ec768de43ebf710215134f9
nooralmustafa2003 <nooralmustafa00@gmail.com>
2025-06-09 23:18:19 -0800

    add button delete and add complte fanction (deletetask,delete,ia actuinresult deletetask

---
+++ DataAccess/TaskRepository.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using NTierTodoApp.Models;
+
+namespace NTierTodoApp.DataAccess
+{
+    /// <summary>
+    /// مستودع البيانات لإدارة المهام باستخدام قائمة في الذاكرة.
+    /// </summary>
+    public class TaskRepository
+    {
+        private List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem { Id = 1, Title = "مهمة أولى", IsComplete = false },
+            new TaskItem { Id = 2, Title = "مهمة ثانية", IsComplete = false }
+        };
+
+        public List<TaskItem> GetAll() => tasks;
+
+        public void Add(TaskItem task)
+        {
+            tasks.Add(task);
+        }
+
+        public TaskItem GetById(int id)
+        {
+            return tasks.FirstOrDefault(t => t.Id == id);
+        }
+
+        // TODO: تنفيذ دالة حذف المهمة
+        public void Delete(int id)
+        {
+            // TODO: ابحث عن المهمة باستخدام id
+        var Deltask =GetById(id);
+                        // TODO: إذا كانت المهمة موجودة، قم بإزالتها من القائمة
+        if( Deltask!=null)
+        {
+            tasks.Remove( Deltask);
+        }
+            
+        }
+    }
+}
